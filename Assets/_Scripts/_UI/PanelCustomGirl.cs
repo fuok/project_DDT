10ab0a4a714af45f7231c6730c773449dd1d4451
fuok <beast_7th@hotmail.com>
2018-02-02 16:09:21 +0800

    add custom name

--- Assets/_Scripts/_UI/PanelCustomGirl.cs
+++ Assets/_Scripts/_UI/PanelCustomGirl.cs
@@ -9,9 +9,19 @@ public class PanelCustomGirl : UIBase
 
 	public Button btnBack;
 	public Toggle toggleCustom;
+
 	//列表item容器
 	public GameObject[] girlItems;
 
+	//文字输入框,先直接用，后面再考虑放进UIBase里
+	public GameObject panelInput;
+	public InputField inputCustomName;
+	public Button btnCustomNameCancel, btnCustomNameConfirm;
+	//输入自定义内容时，保存Playerprefab的key的前缀和序列，同一序列下可能有不同前缀（名字、描述。。。）
+	private int selectIndex;
+	private string selectKey;
+
+	//数据库缓存
 	private List<Girl> mGirlList = new List<Girl> ();
 
 	void Awake ()
@@ -53,26 +63,53 @@ public class PanelCustomGirl : UIBase
 		//列表中的按钮
 		for (int i = 0; i < girlItems.Length; i++) {
 			int tempIndex = i;
+			//头像
 			girlItems [i].transform.Find ("Raw Portrait/Button Picker").GetComponent<Button> ().onClick.AddListener (() => {
 				if (PlayerPrefs.GetInt (Constants.FLAG_CUSTOM_TOGGLE, 0) == 1) {
 					GetComponent<ImagePicker> ().OpenImage (OnImageLoaded, tempIndex);
 				}
 			});
-			
-			//		btnEditName.onClick.AddListener (new UnityEngine.Events.UnityAction ());
-			
-			girlItems [i].transform.Find ("Button Set Default").GetComponent<Button> ().onClick.AddListener (() => {
-				SetItemDefault (tempIndex);
+			//名字
+			girlItems [i].transform.Find ("Button Edit Name").GetComponent<Button> ().onClick.AddListener (() => {
+				if (PlayerPrefs.GetInt (Constants.FLAG_CUSTOM_TOGGLE, 0) == 1) {
+					panelInput.SetActive (true);
+					selectIndex = tempIndex;
+					selectKey = string.Format (Constants.CUSTOM_GIRL_NAME, selectIndex.ToString ());
+				}
 			});
-
-			Refresh (i);
+			//复位
+			girlItems [i].transform.Find ("Button Set Default").GetComponent<Button> ().onClick.AddListener (() => {
+				if (PlayerPrefs.GetInt (Constants.FLAG_CUSTOM_TOGGLE, 0) == 1) {
+					SetItemDefault (tempIndex);
+				}
+			});
+				
 		}
 
+		//Input界面
+		btnCustomNameCancel.onClick.AddListener (() => {
+			inputCustomName.text = "";
+			panelInput.SetActive (false);
+		});
+		btnCustomNameConfirm.onClick.AddListener (() => {
+			if (!string.IsNullOrEmpty (inputCustomName.text)) {
+				PlayerPrefs.SetString (selectKey, inputCustomName.text);
+				Refresh (selectIndex);
+			}
+			inputCustomName.text = "";//顺序别颠倒
+			panelInput.SetActive (false);
+		});
+
+		RefreshAll ();
 	}
 
+	/// <summary>
+	/// 刷新特定item，要把所有可改的内容都放进来
+	/// </summary>
+	/// <param name="index">Index.</param>
 	void Refresh (int index)
 	{
-		print ("Refresh:" + index);
+//		print ("Refresh:" + index);
 		CustomHelper.Instance.SetGirlPortrait (girlItems [index].transform.Find ("Raw Portrait").GetComponent<RawImage> (), mGirlList [index]);
 		girlItems [index].transform.Find ("Text Index").GetComponent<Text> ().text = (index + 1).ToString ();
 		girlItems [index].transform.Find ("Text Name").GetComponent<Text> ().text = CustomHelper.Instance.GetGirlName (mGirlList [index]);
