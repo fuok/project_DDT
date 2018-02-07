35430a5ddbd1832b8184c7e9306610a7bbb2bf59
fuok <beast_7th@hotmail.com>
2018-02-07 18:11:05 +0800

    edit ui

--- Assets/_Scripts/_UI/PanelUiContainer.cs
+++ Assets/_Scripts/_UI/PanelUiContainer.cs
@@ -5,20 +5,20 @@ using UnityEngine.UI;
 
 public class PanelUiContainer : UnitySigleton<PanelUiContainer>
 {
-//	public static PanelUiContainer Instance{ get; private set; }
+	//	public static PanelUiContainer Instance{ get; private set; }
 
 	public Transform[] mPlayerInfoField;
 
 	public Text txtTopMessage;
 
-//	void Awake ()
-//	{
-//		if (Instance == null) {
-//			Instance = this;
-//		} else if (Instance != this) {
-//			Destroy (gameObject);
-//		}
-//	}
+	//	void Awake ()
+	//	{
+	//		if (Instance == null) {
+	//			Instance = this;
+	//		} else if (Instance != this) {
+	//			Destroy (gameObject);
+	//		}
+	//	}
 
 	//	void Start ()
 	//	{
@@ -37,10 +37,16 @@ public class PanelUiContainer : UnitySigleton<PanelUiContainer>
 			if (i < GameManager.Instance.GetAllPlayer ().Count) {
 				mPlayerInfoField [i].gameObject.SetActive (true);
 				//角色属性
-				mPlayerInfoField [i].Find ("name bg/Text Name").GetComponent<Text> ().text = GameManager.Instance.GetAllPlayer () [i].Name;
+				mPlayerInfoField [i].Find ("Text Name").GetComponent<Text> ().text = GameManager.Instance.GetAllPlayer () [i].Name;
 				mPlayerInfoField [i].Find ("Text Money").GetComponent<Text> ().text = GameManager.Instance.GetAllPlayer () [i].Money.ToString ();
 				mPlayerInfoField [i].Find ("Text Girl").GetComponent<Text> ().text = GameManager.Instance.GetPlayerGirl (i).Count.ToString ();
-				mPlayerInfoField [i].Find ("Text Health").GetComponent<Text> ().text = GameManager.Instance.GetAllPlayer () [i].Health.ToString ();
+				mPlayerInfoField [i].Find ("health bg/Text Health").GetComponent<Text> ().text = GameManager.Instance.GetAllPlayer () [i].Health.ToString ();
+				//当前玩家行动高亮
+				if (i == GameManager.Instance.GetCurrentPlayer ().Index) {
+					mPlayerInfoField [i].Find ("highlight").gameObject.SetActive (true);
+				} else {
+					mPlayerInfoField [i].Find ("highlight").gameObject.SetActive (false);
+				}
 			} else {
 				mPlayerInfoField [i].gameObject.SetActive (false);
 			}
